f07058eab53a77bace6061b6398f81a3f4981e7f
Jose Bosquez <jose.bosquez@codeimmersives.com>
2021-10-13 12:56:27 -0600

    update

--- steamstop-backend/app.js
+++ steamstop-backend/app.js
@@ -25,14 +25,14 @@ app.use(passport.initialize())
 
 passport.use('jwt-user', userPassportStrategy)
 
-let originUrl = process.env.NODE_ENV === "development"
-  ? "http://localhost:3000"
-  : "DEPLOY URL";
+// let originUrl = process.env.NODE_ENV === "development"
+//   ? "http://localhost:3000"
+//   : "DEPLOY URL";
 
 const userRouter = require('./routes/users/userRouter')
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
-
+// app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
